c8f1ebc37646ec00cd0b5e87c8ed37c6663e2a58
Daeshawn <dsp341@gmail.com>
2022-03-09 19:19:41 -0600

    Finished :smile:

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pprint
 from array import *
 import numpy as np
-import random as rd
+import time
+
 
 
 def solve(puzzle):
@@ -10,56 +11,93 @@ def solve(puzzle):
     @param puzzle: A 9x9 2D array of integers, a sudoku puzzle
     @return: True if the puzzle can be solved, False otherwise
     """
-    count = 1
-    for x in range(len(puzzle)):
-        for y in range(len(puzzle)):
-            puzzle[x][y] = rd.randint(1,9)
-            count += 1
-        count = 1
 
+    # check if there are still empty boxes
+    empty = findEmpty(puzzle)
+    if not empty:
+        return True
+    else:
+        row = empty[0]  # x
+        col = empty[1]  # y
 
+    for x in range(1, 10):
+        if isValidNumber(puzzle, row, col, x):
+            puzzle[row][col] = x
 
-    #
-    # for x in range(len(puzzle)):
-    #     print(puzzle[x])
-    return True
-
-    # print("This sudoku puzzle cannot be solved")
-    # return False
+            if solve(puzzle):
+                return True
+            # reset to 0 if not valid
+            puzzle[row][col] = 0
+    return False
 
 
 def isValidNumber(puzzle, row, column, number):
+    """
+    checks if there is a conflicting number in the row or column
+    @param puzzle: the sudoku that you are checking
+    @param row: the row in which you want to check for conflicts
+    @param column: the column in which you want to check for conflicts
+    @param number: the number you are trying to check validation
+    @return: True if the number currently fits, False if it does not fit
+    """
 
-    #check row
+    # Check for the same number in the current row
     for x in range(len(puzzle)):
         if puzzle[row][x] == number:
-            print("row error",row, ",", x, "is", number)
+            #print("row error: ",row, ",", x, "already has a", number)
             return False
 
-    #check column
+    # Checks for the current number in the current column
     for x in range(len(puzzle)):
-        if puzzle[x][row] == number:
-            print(row,",", x, "is", number)
+        if puzzle[x][column] == number:
+            #print("column error: ", x,",", column, "already has a ", number)
             return False
+
+    pos = [row, column]
+    # check for the current number in the current 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if puzzle[i][j] == number and (i, j) != pos:
+                return False
 
     return True
 
 
-sudoku =   [[1,2,3,0,5,6,7,8,9],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0]]
+def findEmpty(puzzle):
+    for x in range(len(puzzle)):
+        for y in range(len(puzzle)):
+            if puzzle[x][y] == 0:
+                return (x,y)
+    return
 
 
 
+sudoku =   [[0,0,0,6,0,0,0,3,8],
+           [1,8,6,0,3,7,0,4,2],
+           [7,0,4,8,5,2,0,9,1],
+           [0,0,0,0,7,9,0,0,4],
+           [6,7,0,4,0,5,0,0,0],
+           [0,0,9,0,0,0,8,0,0],
+           [3,0,7,0,0,0,2,0,6],
+           [0,0,0,5,0,0,0,0,7],
+           [8,9,0,0,2,0,4,0,0]]
 
-print(sudoku)
-print(isValidNumber(sudoku,0,1,4))
-#solve(sudoku)
+
+def printPuzzle(puzzle):
+    for x in range(len(puzzle)):
+        print(puzzle[x])
 
 
+def main():
+    print('Unsolved:')
+    printPuzzle(sudoku)
+    solve(sudoku)
+    print('Solved:')
+    printPuzzle(sudoku)
+
+
+if __name__ == '__main__':
+    main()
