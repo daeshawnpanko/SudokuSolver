c600264ebb7a8eddeea1b38c74984eae62c4349e
daeshawnpanko <dsp341@gmail.com>
2022-03-11 12:13:08 -0600

    Final Commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def solve(puzzle):
 
     #
     # for x in range(len(puzzle)):
-    #     print(puzzle[x])
+    print(puzzle[x])
     return True
 
     # print("This sudoku puzzle cannot be solved")
@@ -45,21 +45,21 @@ def isValidNumber(puzzle, row, column, number):
     return True
 
 
-sudoku =   [[1,2,3,0,5,6,7,8,9],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0]]
+sudoku =   [[0,7,4,0,9,0,0,6,1],
+           [9,0,2,1,0,0,3,7,8],
+           [0,0,5,0,0,3,0,2,9],
+           [4,9,0,0,0,0,0,0,2],
+           [2,0,0,9,0,0,7,4,5],
+           [7,0,3,0,0,4,0,0,6],
+           [6,4,9,0,8,0,0,0,7],
+           [5,3,0,7,0,2,9,0,0],
+           [8,0,0,0,0,0,0,0,0]]
 
 
 
 
 print(sudoku)
 print(isValidNumber(sudoku,0,1,4))
-#solve(sudoku)
+solve(sudoku)
 
 
